094046177c4a6d5b5f626df7ae6dcf9f31aa652e
David <davidwfj@gmail.com>
2022-03-24 14:05:44 +0000

    Added Ficha15

---
+++ Ficha15/Ficha15/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Ficha15.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ficha15.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public HomeController( IWebHostEnvironment hostEnvironment)
+        {      
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Image(ImageUploaded image)
+        {
+            return View(image);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost("FileUpload")]
+        public IActionResult Index(IFormFile file)
+        {
+            string path = Path.Combine(this.hostEnvironment.WebRootPath, "images");
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string fileName = Path.GetFileName(file.FileName);
+            using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            {
+                file.CopyTo(stream);                
+                return RedirectToAction("Image", new ImageUploaded { Path = file.FileName });
+            }
+
+            return RedirectToAction("Error");
+        }
+
+    }
+}
---
+++ Ficha15/Ficha15/Models/ImageUploaded.cs
@@ -0,0 +1,7 @@
+﻿namespace Ficha15.Models
+{
+    public class ImageUploaded
+    {
+        public string Path { get; set; }
+    }
+}
